75679c3c45a4b6cab07bcca9dc34cc2ca9a0eba0
gilesdrinkall <giles.drinkall@infinityworks.com>
2019-09-17 08:55:54 +0100

    Second Commit

--- Round.py
+++ Round.py
@@ -8,60 +8,71 @@ def create_dictionary(file_path):
         dict[key] = value
     return dict
 
-people = create_dictionary("people.txt")
-drinks = create_dictionary("drinks.txt")
+peeps = create_dictionary("people.txt")
+bevs = create_dictionary("drinks.txt")
 favourites = create_dictionary("favourites.txt")
 
 pref_mapped={}
 for person_id, drink_id in favourites.items():
-    people_name=people[person_id]
-    drink_name=drinks[drink_id]
-    pref_mapped[people_name]=drink_name
+    people_name = peeps[person_id]
+    drink_name = bevs[drink_id]
+    pref_mapped[people_name] = drink_name
 
 class Round:
 
-    def __init__(self, name_of_maker,favourites):
-        self.name=name_of_maker
-        self.favourites=favourites
-        self.order_ids={}
-        self.round=[]
+    def __init__(self, name_of_maker, favourites):
+        self.name = name_of_maker
+        self.favourites = favourites
+        self.order_ids = {}
+        self.round = []
 
     def get_favourites(self):
         for person, drink in self.favourites.items():
            print(f"  {person} - {drink}")
 
     def display_entries(self, entries):
+        print("\n")
         for id, entry in entries.items():
             print(f"\t{id}\t{entry}")
 
     def get_order_ids(self):
-        input_ids = input("\n\nWhich team members want a drink and what do they want? Enter the respective IDs separated commas:").split(",")
-        for input in input_ids:
-            self.order_ids[input_ids[0]]=input_ids[1]
+        self.order_ids = {}
+        order_open = True
+        while order_open:
+            name_id = input("\nPlease enter the ID of a member of the team that would like a drink: ")
+            drink_id = input('\nPlease enter the ID of the drink they would like: ')
+            self.order_ids[name_id] = drink_id
+            repeat = input("\nDo you want to add another order (yes/no)?")
+            if repeat == "no":
+                order_open = False
 
-    def drinks_list(self):
-        for match in self.order_ids:
-            favourite_text = "\n" + match + " would like " + favourites[match]
-            self.round.append(favourite_text)
-            print(favourite_text)
+    def print_drink_list(self):
+        for pers_id, drk_id in self.order_ids.items():
+            people_name=peeps[pers_id]
+            drink_name=bevs[drk_id]
+            list_text = people_name + " wants " + drink_name + "."
+            self.round.append(list_text)
+            print(list_text)
 
     def persist_round(self):
         file = open("round.txt", "w")
         self.round = "".join(self.round) #Converts list to string
         file.write(self.round)
         file.close()
-        print("Round saved")
+        print("\n\nRound saved!")
+
 
 print("\n\nTime to make a round!")
-round_maker=input("\n\nSo, who's making the round?")
-print(f"\n\nGood on you, {round_maker}!")
+round_maker=input("\nSo, who's making the round?")
+print(f"\nGood on you, {round_maker}!")
 drink_round=Round(round_maker, pref_mapped)
-print("\n\nHere's a list of team members...")
+print("\nHere's a list of team members...")
+drink_round.display_entries(peeps)
+print("\nand here is a menu of available drinks...")
+drink_round.display_entries(bevs)
+drink_round.get_order_ids()
 print("\n")
-drink_round.display_entries(people)
-print("\n\nand here is a menu of available drinks...")
+print(f"Ok, {round_maker}, here's the details of the round: ")
 print("\n")
-drink_round.display_entries(drinks)
-drink_round.get_drinkers()
-drink_round.drinks_list()
+drink_round.print_drink_list()
 drink_round.persist_round()
