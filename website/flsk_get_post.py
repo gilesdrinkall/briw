5647fcef2cf1c4ebb4654919f46c201de7ee7562
gilesdrinkall <giles.drinkall@infinityworks.com>
2019-10-06 15:57:27 +0100

    updated html files

--- website/flsk_get_post.py
+++ website/flsk_get_post.py
@@ -7,20 +7,35 @@ from source.persistence.database import *
 app = Flask(__name__)
 
 
+@app.route("/briwer", methods=["GET", "POST"])
+def add_briwer():
+
+    if request.method == "GET":
+        return render_template("briwer_input.html", title="Create New BrIWer")
+
+    if request.method == "POST":
+        name = request.form["Name of BrIWer"]
+        add_maker_to_db(name)
+        return render_template("briwer_submit.html", briwer_name=name)
+
+    else:
+        return "Unsupported HTTP Request Type"
+
+
 @app.route("/round", methods=["GET", "POST"])
-def view_people():
+def view_add_round():
 
     if request.method == "GET":
         round_dict = get_maker_name_from_db()
         maker_name = round_dict["maker_name"]
         orders_dict = create_dict_with_subset_data_db("orders", "person_name", "drink_name")
-        return render_template("user_input.html", title="Create New Person", round_maker=maker_name, current_order=orders_dict)
+        return render_template("order_input.html", title="Create New Person", round_maker=maker_name, current_order=orders_dict)
 
     if request.method == "POST":
         name = request.form["name"]
         drink = request.form["drink"]
         create_order(name, drink)
-        return render_template("details_submit.html", first_name=name, drink_name=drink)
+        return render_template("order_submit.html", first_name=name, drink_name=drink)
 
     else:
         return "Unsupported HTTP Request Type"
